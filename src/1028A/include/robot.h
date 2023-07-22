60b64c4042293a09f64ada888e15a372913b2ee1
Mustang8192 <mustang08192dev@gmail.com>
2023-07-22 11:22:02 -0500

    setting up odom

--- src/1028A/include/robot.h
+++ src/1028A/include/robot.h
@@ -1,4 +1,5 @@
 #include "api.h"
+#pragma once
 #define leftFrontPort 1
 #define leftMidPort 2
 #define leftBackPort 3
@@ -16,17 +17,26 @@
 extern pros::Motor leftFront;
 extern pros::Motor leftMid;
 extern pros::Motor leftBack;
-extern pros::MotorGroup leftDrive;
+extern pros::Motor_Group leftDrive;
 extern pros::Motor rightFront;
 extern pros::Motor rightMid;
 extern pros::Motor rightBack;
-extern pros::MotorGroup rightDrive;
+extern pros::Motor_Group rightDrive;
 extern pros::Motor AuxL;
 extern pros::Motor AuxR;
-extern pros::MotorGroup Aux;
+extern pros::Motor_Group Aux;
 extern pros::IMU inertial;
 extern pros::GPS gps;
 extern pros::Rotation leftEncoder;
 extern pros::Rotation rightEncoder;
 extern pros::Rotation backEncoder;
 extern pros::Controller mainController;
+
+extern _1028A::TrackingWheel leftTracker;
+extern _1028A::TrackingWheel rightTracker;
+extern _1028A::TrackingWheel backTracker;
+extern _1028A::Drivetrain_t drivetrain;
+extern _1028A::OdomSensors_t OdomSensors;
+extern _1028A::ChassisController_t lateralController;
+extern _1028A::ChassisController_t angularController;
+extern _1028A::Chassis chassis;
