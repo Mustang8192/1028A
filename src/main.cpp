13e634138f88700c142a9eeec5bddfd422e61cc5
Samy Chirumamilla <107894608+Mustang8192@users.noreply.github.com>
2024-11-08 16:29:14 -0600

    1st iteration

--- src/main.cpp
+++ src/main.cpp
@@ -10,13 +10,13 @@ void initialize() {
 void disabled() { }
 
 void competition_initialize() {}
-
+ASSET(Disk6Seq1_txt);
 ASSET(goal1ring_txt);
 
 void autonomous() {
   
   /*_1028A::comp::auton();*/
-  }
+}
 
 void CheckIntakeMotor() {
   const int timeThreshold = 600;
@@ -73,7 +73,7 @@ void opcontrol() {
   _1028A::task::Async Macros(_1028A::comp::driver::macros);
 
   _1028A::robot::intakeMtrs.move(127);
-  pros::delay(220);
+  pros::delay(210);
   _1028A::robot::intakeMtrs.move(0);
   _1028A::robot::chassis.moveToPoint(0, 12, 1000);
   _1028A::robot::chassis.turnToHeading(90, 1000);
@@ -81,12 +81,12 @@ void opcontrol() {
   _1028A::robot::chassis.setPose({-47.283,26.366,180});
   _1028A::robot::mogo.set_value(1);
   pros::delay(200);
-  _1028A::robot::chassis.follow(goal1ring_txt, 10, 10000, true, false);
+  _1028A::robot::chassis.follow(goal1ring_txt, 5, 10000, true, false);
   pros::delay(200);
-  _1028A::robot::chassis.moveToPoint(9, 40.730309, 2000, {.forwards = false}, false);
-  _1028A::robot::chassis.turnToHeading(0, 1000, {}, false);
+  _1028A::robot::chassis.moveToPoint(7, 33.730309, 2000, {.forwards = false}, false);
+  _1028A::robot::chassis.turnToHeading(5, 1000, {}, false);
   _1028A::task::Async diskIndex (MAcro);  
-  _1028A::legacy::forward(180, 127, 1, 1000,0,0);
+  _1028A::legacy::forward(150, 127, 1, 1000,0,0);
     int starttime = pros::millis();
     while (1){
       if(diskIndex.isComplete()){
@@ -100,10 +100,26 @@ void opcontrol() {
       pros::delay(5);
     }
     _1028A::robot::intakeMtrs.move(-127);
-    pros::delay(800);
+    pros::delay(1200);
     _1028A::robot::intakeMtrs.move(0);
-    _1028A::legacy::forward(180, 80, 1, 400,0,0);
+    _1028A::legacy::forward(100, 80, 1, 400,0,0);
     _1028A::robot::intakeMtrs.move(127);
+    pros::delay(200);
+    _1028A::legacy::forward(-150, 127, 1, 1000,0,0);
+    pros::delay(200);
+    _1028A::legacy::turn(-180, 127, 1, 1000,0,0);
+    pros::delay(200);
+    _1028A::robot::leftfront.set_brake_mode(pros::E_MOTOR_BRAKE_COAST);
+    _1028A::robot::leftmid.set_brake_mode(pros::E_MOTOR_BRAKE_COAST);
+    _1028A::robot::leftback.set_brake_mode(pros::E_MOTOR_BRAKE_COAST);
+    _1028A::robot::rightfront.set_brake_mode(pros::E_MOTOR_BRAKE_COAST);
+    _1028A::robot::rightmid.set_brake_mode(pros::E_MOTOR_BRAKE_COAST);
+    _1028A::robot::rightback.set_brake_mode(pros::E_MOTOR_BRAKE_COAST);
+
+  
+    //_1028A::robot::chassis.moveToPoint(-50.977, 42.236, 3000,{.maxSpeed = 100}, false);
+    
+   
 
   while (true) {
     pros::delay(200);
